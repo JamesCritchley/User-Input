d45f04d90ed4daa24f3669c8b736eb53e9562abf
JamesCritchley <jcritchley@utcmediacityuk.org.uk>
2016-09-15 08:49:49 +0100

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,46 @@
+
+package userinput;
+import java.util.Scanner;
+
+public class UserInput {
+
+   
+    public static void main(String[] args) {
+    
+  
+    
+        System.out.println("Please enter the following information so I can sell it for a profit");
+         Scanner sc = new Scanner (System. in);      
+        System.out.println("First Name: ");
+        String FirstName = sc.nextLine();
+        System.out.println("Last Name: ");
+        String Surname = sc.nextLine ();
+        System.out.println("Grade: "); 
+        String Grade = sc.nextLine ();
+        System.out.println("Student ID: ");
+        String StudentID = sc.nextLine ();
+        System.out.println("Login: ");
+        String Login = sc.nextLine ();
+        System.out.println("GPA: ");
+        String GPA = sc.nextLine ();
+          
+        int X = 50;
+        int Y = 25;
+        if (X<Y);
+        System.out.println("Y is bigger than X");
+          }else{
+            System.out.println("X is bigger than Y");
+            
+            while (x > 0) {
+                System.out.println("x");
+                x = x - 1;
+            }
+         int a = 10;
+         int b = 5;
+            System.out.println(a%b);
+    
+    
+    }
+    
+    
+}
